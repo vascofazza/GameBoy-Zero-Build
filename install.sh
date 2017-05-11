75884bfc31c8587c93fe22461894f5eb38304929
vascofazza <fscozzafava@imacdifederico.lan>
2017-05-12 00:07:34 +0200

    conditional install a lot of improvements and features

--- install.sh
+++ install.sh
@@ -21,14 +21,25 @@ fi
 
 #Configurations
 echo "Installing configurations"
-sed -i -- 's/console=tty1/console=tty3/g' /boot/cmdline.txt #disable start/shutdown text
+if ! grep -Fxq "console=tty1" /boot/cmdline.txt
+then
+    sed -i -- 's/console=tty1/console=tty3/g' /boot/cmdline.txt #disable start/shutdown text
+fi
+if ! grep -Fxq "logo.nologo" /boot/cmdline.txt
+then
+echo -n " logo.nologo" >> /boot/cmdline.txt #disable start/shutdown text
+fi
 cp configs/config.txt /boot/config.txt
 # Insert shutdown script into rc.local before final 'exit 0'
-sed -i "s/^exit 0/bash \/home\/pi\/scripts\/screen.sh \&\\nexit 0/g" /etc/rc.local >/dev/null
+if ! grep -Fxq "bash \/home\/pi\/scripts\/screen.sh" /etc/rc.local
+then
+    sed -i "s/^exit 0/bash \/home\/pi\/scripts\/screen.sh \&\\nexit 0/g" /etc/rc.local >/dev/null
+fi
 # Insert shutdown script into rc.local before final 'exit 0'
-sed -i "s/^exit 0/python \/home\/pi\/scripts\/shutdown.py \&\\nexit 0/g" /etc/rc.local >/dev/null
-# Insert battery script into rc.local before final 'exit 0'
-#sed -i "s/^exit 0/python \/home\/pi\/scripts\/battery.py \&\\nexit 0/g" /etc/rc.local >/dev/null
+if ! grep -Fxq "python \/home\/pi\/scripts\/shutdown.py" /etc/rc.local
+then
+    sed -i "s/^exit 0/python \/home\/pi\/scripts\/shutdown.py \&\\nexit 0/g" /etc/rc.local >/dev/null
+fi
 
 ##battery meter stuff
 echo "Installing libraries"
@@ -47,10 +58,29 @@ mkdir /home/pi/scripts
 chown pi /home/pi/scripts
 cp scripts/screen.sh /home/pi/scripts/screen.sh
 cp scripts/shutdown.py /home/pi/scripts/shutdown.py
-#cp scripts/battery.py /home/pi/scripts/battery.py
 chmod 755 scripts/shutdown.py
 chmod 755 scripts/screen.sh
-#chmod 755 scripts/battery.py
+
+#Battery monitor
+read -p "Do you want to install the Battery Meter Script?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+mkdir gbzattinymonitor
+cd gbzattinymonitor/
+wget https://raw.githubusercontent.com/vascofazza/gbzattinymonitor/master/Install.sh
+chmod +x install.sh
+bash install.sh
+fi
+
+#fixed version of pifba
+read -p "Do you want to download Lumberjack's pifba fixed version for NEOGEO?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+wget https://github.com/digitalLumberjack/pifba/releases/download/0.3/pifba-0.3.zip -P /home/pi/
+fi
+
 
 echo "The system will reboot in 5 seconds"
 sleep 5
